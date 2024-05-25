aaaa4fa66616afee2d61ffe51adb30f09d510e0d
Duy Tran <duytran31187@gmail.com>
2024-05-25 11:24:08 +0700

    render lich phung vu in node

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Lich phung vu' });
 });
 
 module.exports = router;

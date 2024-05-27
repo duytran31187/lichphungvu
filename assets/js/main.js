38d9dcf573a09d9a70384550f8c62bfc07e8bf2f
Duy Tran <duytran31187@gmail.com>
2024-05-27 21:25:04 +0700

    show full month

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,38 @@
-import { tinhNamPhungVu, nameOfDays } from './dist/index.mjs'
+import { nameOfDays, tinhNamPhungVu } from './dist/index.mjs'
 ////  
 $(document).ready(function () {
-  var date = new Date();
-  var month = date.getMonth(); //months from 1-12
-  var year = date.getFullYear();
-  var firstDay = new Date(year, month, 1);
-  var lastDay = new Date(year, month + 1, 0);
-  var calendar = document.getElementById('calendar');
-  var calendarTitle = document.getElementById('calender-title');
-  if (calendarTitle) {
-    calendarTitle.innerHTML = date.toISOString().substring(0,7);
-  }
-  var lichphungvuTable = document.getElementById('lichphungvuTable');
-  for (var i = firstDay.getDay(); i > 0; i--) {
-      var cell = document.createElement('td');
-      cell.textContent = ' ';
-      calendar.appendChild(cell);
-  }
 
-  for (var i = 1; i <= lastDay.getDate(); i++) {
-      if ((i + firstDay.getDay() - 1) % 7 === 0) {
-          var row = document.createElement('tr');
-          calendar.appendChild(row);
-      }
-      var cell = document.createElement('td');
-      cell.textContent = i;
-      calendar.appendChild(cell);
+  // add lich phung vu
+  const printCalendarMonth = (year, month) => {
+    const calendar = document.createElement('table');
+    const thead = document.createElement('thead');
+    const headTr = document.createElement('tr');
+    for(let wd of ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']) {
+      const th = document.createElement('th');
+      th.innerText = wd;
+      headTr.appendChild(th);
+    }
+    thead.appendChild(headTr);
+    calendar.appendChild(thead);
+    // print date
+    var firstDay = new Date(year, month-1, 1);
+    var lastDay = new Date(year, month, 0);
+    for (var i = firstDay.getDay(); i > 0; i--) {
+        var cell = document.createElement('td');
+        cell.textContent = ' ';
+        calendar.appendChild(cell);
+    }
+
+    for (var i = 1; i <= lastDay.getDate(); i++) {
+        if ((i + firstDay.getDay() - 1) % 7 === 0) {
+            var row = document.createElement('tr');
+            calendar.appendChild(row);
+        }
+        var cell = document.createElement('td');
+        cell.textContent = i;
+        calendar.appendChild(cell);
+    }
+    return calendar;
   }
   
   var currentDate = new Date();
@@ -39,21 +46,83 @@ $(document).ready(function () {
 
   var date = new Date(searchYear + '-' + currentDate.getMonth() + '-01');
   var y = date.getFullYear();
+
   var namphungVuIns = tinhNamPhungVu(y);
   
-  let count = 0;
-  for( let key in namphungVuIns) 
-  {
+  const printHeadOfMonth = (month) => {
     const trItemBody =document.createElement('tr');
-    const tdItemLabel = document.createElement('th');
-    tdItemLabel.textContent = nameOfDays[count];
-    trItemBody.appendChild(tdItemLabel);
-
-    const tdItemVal = document.createElement('td');
-    tdItemVal.textContent = namphungVuIns[key] instanceof Date ? namphungVuIns[key].toDateString() : namphungVuIns[key];
-    trItemBody.appendChild(tdItemVal);
+    const th = document.createElement('th');
+    th.colSpan = 3;
+    th.className = 'th-month';
+    th.textContent = 'thang ' + month;
+    trItemBody.appendChild(th);
     lichphungvuTable.appendChild(trItemBody);
-    count++;
   }
-  
+  const printABC = () => {
+    const trItemBody =document.createElement('tr');
+    const th = document.createElement('th');
+    th.colSpan = 3;
+    th.className = 'summary_month';
+    th.textContent = namphungVuIns.year + ' - Năm ' + namphungVuIns.yearABC + ' - ' + namphungVuIns.oddEven;
+    trItemBody.appendChild(th);
+    lichphungvuTable.appendChild(trItemBody);
+  }
+  const printByMonth = (month) => {
+    printHeadOfMonth(month);
+    
+    const monthInNum = month-1;
+    const firstDay = new Date(namphungVuIns.year, monthInNum, 1);
+    const lastDay = new Date(namphungVuIns.year, month, 0);
+    const ngayPhungVuTheoThang = [];
+    console.log( 'last ' + lastDay.toDateString());
+    for( let key in namphungVuIns) 
+      {
+        if (namphungVuIns[key] instanceof Date && namphungVuIns[key].getMonth() == monthInNum) {
+          ngayPhungVuTheoThang[namphungVuIns[key].toDateString()] = nameOfDays[key];
+          // const trItemBody =document.createElement('tr');
+          // const td1 = document.createElement('td');
+          // const td2 = document.createElement('td');
+          // const td3 = document.createElement('td');
+          // td1.innerText = namphungVuIns[key].toDateString();
+          // td2.innerText = namphungVuIns[key].toDateString();
+          // td3.innerText = nameOfDays[key];
+          // trItemBody.appendChild(td1);
+          // trItemBody.appendChild(td2);
+          // trItemBody.appendChild(td3);
+          // lichphungvuTable.appendChild(trItemBody);
+        }
+      }
+      for (let d = 0; d < 32; d++ ) {
+        const currentDate = new Date(firstDay.getTime());
+        currentDate.setDate(currentDate.getDate() + d);
+        if (currentDate.getTime() <= lastDay.getTime()) { // if > last day => skip
+          const trItemBody =document.createElement('tr');
+          const td1 = document.createElement('td');
+          const td2 = document.createElement('td');
+          const td3 = document.createElement('td');
+          td1.innerText = (d+1); // ngay 1/thang
+          if (ngayPhungVuTheoThang[currentDate.toDateString()]) {
+            td2.innerText = currentDate.toDateString();
+            td3.innerText = ngayPhungVuTheoThang[currentDate.toDateString()];
+          }
+          trItemBody.appendChild(td1);
+          trItemBody.appendChild(td2);
+          trItemBody.appendChild(td3);
+          lichphungvuTable.appendChild(trItemBody);
+        }
+      }
+  }
+  printABC();
+  for (let i=1; i<=12;i++) {
+    printByMonth(i);
+  }
+  // print full calendar by year
+  // for( let imonth=1; imonth<=12;imonth++) {
+  //   const divCalendar = document.getElementById('divCalendar');
+  //   const h5 = document.createElement('h5');
+  //   h5.innerText = 'Thang '+ imonth + ' / ' + searchYear;
+  //   divCalendar.appendChild(h5);
+  //   const fullMonth = printCalendarMonth(searchYear, imonth);
+  //   divCalendar.appendChild(fullMonth);
+  // }
 });
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,62 +1,39 @@
-import { tinhNamPhungVu } from './dist/index.mjs'
+import { nameOfDays, tinhNamPhungVu } from './dist/index.mjs'
 ////  
 $(document).ready(function () {
-  var date = new Date();
-  var month = date.getMonth(); //months from 1-12
-  var year = date.getFullYear();
-  var firstDay = new Date(year, month, 1);
-  var lastDay = new Date(year, month + 1, 0);
-  var calendar = document.getElementById('calendar');
-  var calendarTitle = document.getElementById('calender-title');
-  if (calendarTitle) {
-    calendarTitle.innerHTML = date.toISOString().substring(0,7);
-  }
-  var lichphungvuTable = document.getElementById('lichphungvuTable');
-  for (var i = firstDay.getDay(); i > 0; i--) {
-      var cell = document.createElement('td');
-      cell.textContent = ' ';
-      calendar.appendChild(cell);
-  }
-
-  for (var i = 1; i <= lastDay.getDate(); i++) {
-      if ((i + firstDay.getDay() - 1) % 7 === 0) {
-          var row = document.createElement('tr');
-          calendar.appendChild(row);
-      }
-      var cell = document.createElement('td');
-      cell.textContent = i;
-      calendar.appendChild(cell);
-  }
 
   // add lich phung vu
-  var colums = [
-      'year( Năm)',
-      'A|B|C (năm A|B|C)',
-      'Odd|Even (Năm chẵn lẻ)',
-      'The Epiphany of the Lord (Lễ Chúa Hiển Linh)',
-      'Lễ Chúa chịu phép rửa',
-      'Ash Wednesday (Thứ tư lễ tro)',
-      'First Sunday of Lent (Chúa nhật thứ nhất mùa chay)',
-      'Second Sunday of Lent (Chúa nhật thứ 2 mùa chay)',
-      'Third Sunday of Lent (Chúa nhật thứ 3 mùa chay)',
-      'Fourth Sunday of Lent (Chúa nhật thứ 4 mùa chay)',
-      'Fifth Sunday of Lent (Chúa nhật thứ 5 mùa chay)',
-      'Palm Sunday (Lễ Lá)',
-      'Easter Sunda (Phục sinh)',
-      'Second Sunday of Easter (Chúa nhật thứ 2 phục sinh)',
-      'Third Sunday of Easter (Chúa nhật thứ 3 phục sinh)',
-      'Fourth Sunday of Easter (Chúa nhật thứ 4 phục sinh)',
-      'Fifth Sunday of Easter (Chúa nhật thứ 5 phục sinh)',
-      'Sixth Sunday of Easter (Chúa nhật thứ 6 phục sinh)',
-      'The Ascention of the Lord (Lễ Chúa Lên Trời)',
-      'Pentecost Sunday (Lễ Chúa Thánh Thần hiện xuống)',
-      'First Sunday of Advent (Chúa nhật thứ nhất mùa vọng)',
-      'Second Sunday of Advent (Chúa nhật thứ 2 mùa vọng)',
-      'Third Sunday of Advent (Chúa nhật thứ 3 mùa vọng)',
-      'Fourth Sunday of Advent (Chúa nhật thứ tư mùa vọng)',
-      'Christmas (Giáng sinh)',
-      'Lễ Thánh Gia'
-  ];
+  const printCalendarMonth = (year, month) => {
+    const calendar = document.createElement('table');
+    const thead = document.createElement('thead');
+    const headTr = document.createElement('tr');
+    for(let wd of ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']) {
+      const th = document.createElement('th');
+      th.innerText = wd;
+      headTr.appendChild(th);
+    }
+    thead.appendChild(headTr);
+    calendar.appendChild(thead);
+    // print date
+    var firstDay = new Date(year, month-1, 1);
+    var lastDay = new Date(year, month, 0);
+    for (var i = firstDay.getDay(); i > 0; i--) {
+        var cell = document.createElement('td');
+        cell.textContent = ' ';
+        calendar.appendChild(cell);
+    }
+
+    for (var i = 1; i <= lastDay.getDate(); i++) {
+        if ((i + firstDay.getDay() - 1) % 7 === 0) {
+            var row = document.createElement('tr');
+            calendar.appendChild(row);
+        }
+        var cell = document.createElement('td');
+        cell.textContent = i;
+        calendar.appendChild(cell);
+    }
+    return calendar;
+  }
   
   var currentDate = new Date();
   var defaultYear = currentDate.getFullYear();
@@ -69,36 +46,83 @@ $(document).ready(function () {
 
   var date = new Date(searchYear + '-' + currentDate.getMonth() + '-01');
   var y = date.getFullYear();
-//   var tuanmuaVong = tinh4TuanMuaVong(y);
-//   var year = tuanmuaVong.yearABC;
-//   var easter = tinhNgayPhucSinh(y);
-//   var ashWednesday = tinhThuTuLeTro(easter);
+
   var namphungVuIns = tinhNamPhungVu(y);
   
-  // const trItem =document.createElement('tr');
-  // for( let key in colums) 
-  // {
-  //   const tdItem = document.createElement('td');
-  //   tdItem.textContent = colums[key];
-  //   trItem.appendChild(tdItem);
-  // }
-  // lichphungvuTable.appendChild(trItem);
-    
-  
-  
-  let count = 0;
-  for( let key in namphungVuIns) 
-  {
+  const printHeadOfMonth = (month) => {
     const trItemBody =document.createElement('tr');
-    const tdItemLabel = document.createElement('th');
-    tdItemLabel.textContent = colums[count];
-    trItemBody.appendChild(tdItemLabel);
-
-    const tdItemVal = document.createElement('td');
-    tdItemVal.textContent = namphungVuIns[key] instanceof Date ? namphungVuIns[key].toDateString() : namphungVuIns[key];
-    trItemBody.appendChild(tdItemVal);
+    const th = document.createElement('th');
+    th.colSpan = 3;
+    th.className = 'th-month';
+    th.textContent = 'thang ' + month;
+    trItemBody.appendChild(th);
     lichphungvuTable.appendChild(trItemBody);
-    count++;
   }
-  
+  const printABC = () => {
+    const trItemBody =document.createElement('tr');
+    const th = document.createElement('th');
+    th.colSpan = 3;
+    th.className = 'summary_month';
+    th.textContent = namphungVuIns.year + ' - Năm ' + namphungVuIns.yearABC + ' - ' + namphungVuIns.oddEven;
+    trItemBody.appendChild(th);
+    lichphungvuTable.appendChild(trItemBody);
+  }
+  const printByMonth = (month) => {
+    printHeadOfMonth(month);
+    
+    const monthInNum = month-1;
+    const firstDay = new Date(namphungVuIns.year, monthInNum, 1);
+    const lastDay = new Date(namphungVuIns.year, month, 0);
+    const ngayPhungVuTheoThang = [];
+    console.log( 'last ' + lastDay.toDateString());
+    for( let key in namphungVuIns) 
+      {
+        if (namphungVuIns[key] instanceof Date && namphungVuIns[key].getMonth() == monthInNum) {
+          ngayPhungVuTheoThang[namphungVuIns[key].toDateString()] = nameOfDays[key];
+          // const trItemBody =document.createElement('tr');
+          // const td1 = document.createElement('td');
+          // const td2 = document.createElement('td');
+          // const td3 = document.createElement('td');
+          // td1.innerText = namphungVuIns[key].toDateString();
+          // td2.innerText = namphungVuIns[key].toDateString();
+          // td3.innerText = nameOfDays[key];
+          // trItemBody.appendChild(td1);
+          // trItemBody.appendChild(td2);
+          // trItemBody.appendChild(td3);
+          // lichphungvuTable.appendChild(trItemBody);
+        }
+      }
+      for (let d = 0; d < 32; d++ ) {
+        const currentDate = new Date(firstDay.getTime());
+        currentDate.setDate(currentDate.getDate() + d);
+        if (currentDate.getTime() <= lastDay.getTime()) { // if > last day => skip
+          const trItemBody =document.createElement('tr');
+          const td1 = document.createElement('td');
+          const td2 = document.createElement('td');
+          const td3 = document.createElement('td');
+          td1.innerText = (d+1); // ngay 1/thang
+          if (ngayPhungVuTheoThang[currentDate.toDateString()]) {
+            td2.innerText = currentDate.toDateString();
+            td3.innerText = ngayPhungVuTheoThang[currentDate.toDateString()];
+          }
+          trItemBody.appendChild(td1);
+          trItemBody.appendChild(td2);
+          trItemBody.appendChild(td3);
+          lichphungvuTable.appendChild(trItemBody);
+        }
+      }
+  }
+  printABC();
+  for (let i=1; i<=12;i++) {
+    printByMonth(i);
+  }
+  // print full calendar by year
+  // for( let imonth=1; imonth<=12;imonth++) {
+  //   const divCalendar = document.getElementById('divCalendar');
+  //   const h5 = document.createElement('h5');
+  //   h5.innerText = 'Thang '+ imonth + ' / ' + searchYear;
+  //   divCalendar.appendChild(h5);
+  //   const fullMonth = printCalendarMonth(searchYear, imonth);
+  //   divCalendar.appendChild(fullMonth);
+  // }
 });

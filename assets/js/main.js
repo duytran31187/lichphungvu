b3dd1cb0877bc94025b0fa8eaf1096f9d93204a1
Duy Tran <duytran31187@gmail.com>
2024-06-12 21:24:50 +0700

    name

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,8 +28,7 @@ $(document).ready(function () {
 
   var date = new Date(searchYear + '-' + currentDate.getMonth() + '-01');
   
-  console.log(searchYear);
-  let namPhungvu = getTinhNamPhungVuInstant(searchYear);
+  var namPhungvu = getTinhNamPhungVuInstant(searchYear);
   var namphungVuIns = namPhungvu.getNamPhungVu();
   
   const printHeadOfMonth = (month) => {
@@ -50,68 +49,9 @@ $(document).ready(function () {
     trItemBody.appendChild(th);
     lichphungvuTable.appendChild(trItemBody);
   }
-  const printByMonth = (month, chuaNhatTuanThuongNien, chuaNhatThuongNienMua2) => {
-    printHeadOfMonth(month);
-    const classMonth = month%2 == 1 ? 'odd-cls' : 'even-cls';
-    const monthInNum = month-1;
-    const firstDay = new Date(namphungVuIns.year, monthInNum, 1);
-    const lastDay = new Date(namphungVuIns.year, month, 0);
-    const ngayPhungVuTheoThang = [];
-    // lay danh sach ngay phung vu từ library
-    const leChuaThanhThanHienxuong = namphungVuIns.pentecostSunday;
-    const chuaChiuPhepRua = namphungVuIns.leChuaChiuPhepRua;
-    const leTro = namphungVuIns.ashWed;
-    for( let key in namphungVuIns) 
-    {
-      
-      if (namphungVuIns[key] instanceof Date && namphungVuIns[key].getMonth() == monthInNum) {
-        ngayPhungVuTheoThang[namphungVuIns[key].toDateString()] = nameOfDays[key];
-      }
-    }
-    for (let d = 0; d < 32; d++ ) {
-      const currentDate = new Date(firstDay.getTime());
-      currentDate.setDate(currentDate.getDate() + d);
-      if (currentDate.getTime() <= lastDay.getTime()) { // if > last day => skip
-        const trItemBody =document.createElement('tr');
-        trItemBody.className = weekday[currentDate.getDay()] + ' ' + classMonth; // set class name theo weekday
-        const td1 = document.createElement('td');
-        // const td2 = document.createElement('td');
-        const td3 = document.createElement('td');
-        td1.innerText = printDate(currentDate); // ngay 1/thang
-        if (ngayPhungVuTheoThang[currentDate.toDateString()]) { // neu co trong danh sach in ra
-          // td2.innerText = currentDate.toDateString();
-          td3.innerText = ngayPhungVuTheoThang[currentDate.toDateString()];
-        } else {
-          if (
-            currentDate.getDay() == 0 // sunday
-            && currentDate.getTime() > leChuaThanhThanHienxuong.getTime() // sau le chua thanh than hien xuong
-          ) {
-            // td2.innerText = currentDate.toDateString();
-            td3.innerText = 'Chua Nhat thu ' + chuaNhatThuongNienMua2 + ' mua Thuong Nien';
-            chuaNhatThuongNienMua2++;
-          } else if (
-            currentDate.getDay() == 0 // sunday
-            && currentDate.getTime() > chuaChiuPhepRua.getTime() // chua nhat dau tien sau chua chiu phep rua la tuan 2 thuogn nien
-            && currentDate.getTime() < leTro.getTime()
-          ) {
-            td3.innerText = 'Chua Nhat thu ' + chuaNhatTuanThuongNien + ' mua Thuong Nien';
-            chuaNhatTuanThuongNien++;
-          }
-        }
-        trItemBody.appendChild(td1);
-        // trItemBody.appendChild(td2);
-        trItemBody.appendChild(td3);
-        lichphungvuTable.appendChild(trItemBody);
-      }
-    }
-    return [
-      chuaNhatTuanThuongNien,
-      chuaNhatThuongNienMua2
-    ];
-  }
   printABC();
   
-  const printFullLichPhungVuTheoNam = (year) => {
+  const printFullLichPhungVuTheoNam = () => {
     const fullYear = namPhungvu.getFullLichPhungVuTheoNam();
     let currentMonth = 0;
     // printHeadOfMonth(currentMonth);
@@ -147,5 +87,5 @@ $(document).ready(function () {
     }
     
   };
-  printFullLichPhungVuTheoNam(currentDate.getFullYear());
+  printFullLichPhungVuTheoNam();
 });

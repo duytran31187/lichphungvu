42746c12bd265992bfad7428983ba03a29f8798b
Duy Tran <duytran31187@gmail.com>
2024-06-19 17:02:43 +0700

    debug mobile

--- assets/js/main-mobile.js
+++ assets/js/main-mobile.js
@@ -1,4 +1,4 @@
-import {  LE_KINH, LE_NHO, LE_TRONG, getTinhNamPhungVuInstant, nameOfDays } from './dist-mobile/index.mjs'
+import {  getTinhNamPhungVuInstant } from './dist-mobile/index.mjs'
 
 var namPhungvu = getTinhNamPhungVuInstant(2024);
 const fullYear = namPhungvu.getFullLichPhungVuTheoNam();
@@ -19,13 +19,6 @@ for (let yk in fullYear) {
       // }
       label += fullYear[yk]['cacNgayLe'][ngayle]['name'];
       let typeClss = '';
-      if (typeNgayLe == LE_NHO) {
-          typeClss = 'le_nho_d';
-      } else if (typeNgayLe == LE_KINH) {
-          typeClss = 'le_kinh_d';
-      } else if (typeNgayLe == LE_TRONG) {
-          typeClss = 'le_trong_d';
-      }
       document.getElementById('holder').innerHTML = label;
       count++;
       // data.push(

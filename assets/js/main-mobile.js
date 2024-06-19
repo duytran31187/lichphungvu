bc7082f4836efff7712fd29b1754de73748b07dc
Duy Tran <duytran31187@gmail.com>
2024-06-19 16:18:44 +0700

    mobile

--- assets/js/main-mobile.js
+++ assets/js/main-mobile.js
@@ -214,51 +214,13 @@ import {  LE_KINH, LE_NHO, LE_TRONG, getTinhNamPhungVuInstant, nameOfDays } from
       mode: "month",
       data: []
     }, jQuery, window, document);
-
+    
+    // in lich phung vu
+    $('#holder').calendar({
+      data: data
+    });
   })(jQuery);
 
   var data = [];
 
 
-// in lich phung vu
-for (let y = 2020; y <=2030;y++) { // add 10 nam
-  var namPhungvu = getTinhNamPhungVuInstant(y);
-  const fullYear = namPhungvu.getFullLichPhungVuTheoNam();
-  for (let yk in fullYear) {
-      for (let ngayle in fullYear[yk]['cacNgayLe']) {
-          const tmpD = fullYear[yk]['date'];
-          tmpD.setHours(0);
-          tmpD.setMinutes(0);
-          tmpD.setSeconds(0);
-          let label = '';
-          const typeNgayLe = fullYear[yk]['cacNgayLe'][ngayle]['type'];
-          // if (fullYear[yk]['cacNgayLe'][ngayle]['type'] != '') {
-          //     label = fullYear[yk]['cacNgayLe'][ngayle]['type'] +'|';
-          // }
-          label += fullYear[yk]['cacNgayLe'][ngayle]['name'];
-          let typeClss = '';
-          if (typeNgayLe == LE_NHO) {
-              typeClss = 'le_nho_d';
-          } else if (typeNgayLe == LE_KINH) {
-              typeClss = 'le_kinh_d';
-          } else if (typeNgayLe == LE_TRONG) {
-              typeClss = 'le_trong_d';
-          }
-          data.push(
-              {
-              title: label,
-              start: tmpD,
-              end: tmpD,
-              typeClss: typeClss,
-              allDay: true
-              }
-          );
-      }
-  }
-}
-
-data.sort(function (a, b) { return (+a.start) - (+b.start); });
-//Actually do everything
-$('#holder').calendar({
-  data: data
-});

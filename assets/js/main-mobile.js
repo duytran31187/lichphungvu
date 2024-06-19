400ba840ad2d65c1af17aa70ffde8ae1f3e95a42
Duy Tran <duytran31187@gmail.com>
2024-06-19 17:30:50 +0700

    66

--- assets/js/main-mobile.js
+++ assets/js/main-mobile.js
@@ -1,35 +1,35 @@
-//import {  getTinhNamPhungVuInstant } from './dist-mobile/index.mjs'
+import {  getTinhNamPhungVuInstant } from './dist-mobile/index.mjs'
 
-//var namPhungvu = getTinhNamPhungVuInstant(2024);
-// const fullYear = namPhungvu.getFullLichPhungVuTheoNam();
+var namPhungvu = getTinhNamPhungVuInstant(2024);
+const fullYear = namPhungvu.getFullLichPhungVuTheoNam();
 let count = 0;
 document.getElementById('holder').innerHTML = 'aaaaaaaaa';
-// for (let yk in fullYear) {
-//   if(count >=2) {
-//     break;
-//   }
-//   for (let ngayle in fullYear[yk]['cacNgayLe']) {
-//       const tmpD = fullYear[yk]['date'];
-//       tmpD.setHours(0);
-//       tmpD.setMinutes(0);
-//       tmpD.setSeconds(0);
-//       let label = '';
-//       const typeNgayLe = fullYear[yk]['cacNgayLe'][ngayle]['type'];
-//       // if (fullYear[yk]['cacNgayLe'][ngayle]['type'] != '') {
-//       //     label = fullYear[yk]['cacNgayLe'][ngayle]['type'] +'|';
-//       // }
-//       label += fullYear[yk]['cacNgayLe'][ngayle]['name'];
-//       let typeClss = '';
-//       document.getElementById('holder').innerHTML = label;
-//       count++;
-//       // data.push(
-//       //     {
-//       //     title: label,
-//       //     start: tmpD,
-//       //     end: tmpD,
-//       //     typeClss: typeClss,
-//       //     allDay: true
-//       //     }
-//       // );
-//   }
-// }
+for (let yk in fullYear) {
+  if(count >=2) {
+    break;
+  }
+  for (let ngayle in fullYear[yk]['cacNgayLe']) {
+      const tmpD = fullYear[yk]['date'];
+      tmpD.setHours(0);
+      tmpD.setMinutes(0);
+      tmpD.setSeconds(0);
+      let label = '';
+      const typeNgayLe = fullYear[yk]['cacNgayLe'][ngayle]['type'];
+      // if (fullYear[yk]['cacNgayLe'][ngayle]['type'] != '') {
+      //     label = fullYear[yk]['cacNgayLe'][ngayle]['type'] +'|';
+      // }
+      label += fullYear[yk]['cacNgayLe'][ngayle]['name'];
+      let typeClss = '';
+      document.getElementById('holder').innerHTML = label;
+      count++;
+      // data.push(
+      //     {
+      //     title: label,
+      //     start: tmpD,
+      //     end: tmpD,
+      //     typeClss: typeClss,
+      //     allDay: true
+      //     }
+      // );
+  }
+}

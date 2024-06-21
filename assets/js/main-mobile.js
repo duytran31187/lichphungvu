c654fd4ccdd17b8c7714668bd14aa9ebd752d10a
Duy Tran <duytran31187@gmail.com>
2024-06-21 18:02:44 +0700

    41234123

--- assets/js/main-mobile.js
+++ assets/js/main-mobile.js
@@ -1,5 +1,3 @@
-import {  getTinhNamPhungVuInstant } from './dist-mobile/index.mjs'
-
 var namPhungvu = getTinhNamPhungVuInstant(2024);
 const fullYear = namPhungvu.getFullLichPhungVuTheoNam();
 let count = 0;

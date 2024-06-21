f2055b575da40fbc9a0c9cb056220655227a9fa5
Duy Tran <duytran31187@gmail.com>
2024-06-21 17:33:03 +0700

    5

--- assets/js/main-mobile.js
+++ assets/js/main-mobile.js
@@ -4,37 +4,3 @@
 // const fullYear = namPhungvu.getFullLichPhungVuTheoNam();
 let count = 0;
 document.getElementById('holder3').innerHTML = 'start333333333333333....';
-// for (let yk in fullYear) {
-//   if(count >=2) {
-//     break;
-//   }
-//   for (let ngayle in fullYear[yk]['cacNgayLe']) {
-//       const tmpD = fullYear[yk]['date'];
-//       tmpD.setHours(0);
-//       tmpD.setMinutes(0);
-//       tmpD.setSeconds(0);
-//       let label = '';
-//       const typeNgayLe = fullYear[yk]['cacNgayLe'][ngayle]['type'];
-//       // if (fullYear[yk]['cacNgayLe'][ngayle]['type'] != '') {
-//       //     label = fullYear[yk]['cacNgayLe'][ngayle]['type'] +'|';
-//       // }
-//       label += fullYear[yk]['cacNgayLe'][ngayle]['name'];
-//       let typeClss = '';
-//       divItem.innerHTML =  label;
-//       count++;
-//       data.push(
-//           {
-//           title: label,
-//           start: tmpD,
-//           end: tmpD,
-//           typeClss: typeClss,
-//           allDay: true
-//           }
-//       );
-//   }
-// }
-// (function ($) {
-//     $('#holder').calendar({
-//         data: data
-//       });
-// });

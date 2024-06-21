6883b19b3df8f8d43c9cc534129f2e01dc039f30
Duy Tran <duytran31187@gmail.com>
2024-06-21 17:02:19 +0700

    3

--- assets/js/main-mobile.js
+++ assets/js/main-mobile.js
@@ -1,7 +1,7 @@
-// import {  getTinhNamPhungVuInstant } from './dist-mobile/index.mjs'
+import {  getTinhNamPhungVuInstant } from './dist-mobile/index.mjs'
 
-// var namPhungvu = getTinhNamPhungVuInstant(2024);
-// const fullYear = namPhungvu.getFullLichPhungVuTheoNam();
+var namPhungvu = getTinhNamPhungVuInstant(2024);
+const fullYear = namPhungvu.getFullLichPhungVuTheoNam();
 let count = 0;
 document.getElementById('holder').innerHTML = 'start....';
 // for (let yk in fullYear) {

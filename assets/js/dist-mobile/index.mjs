4bece7fc9926b56886fe0a975861ac406658f0cd
Duy Tran <duytran31187@gmail.com>
2024-06-22 18:59:43 +0700

    qer

--- assets/js/dist-mobile/index.mjs
+++ assets/js/dist-mobile/index.mjs
@@ -1,5 +1,25 @@
+function tinhNamABC(y) {
+  let yearStr = y.toString();
+  let yearNums = Array.from(yearStr);
+  let countNum = 0;
+  let year;
+  yearNums.forEach((element) => {
+    countNum += parseInt(element);
+  });
+  switch (countNum % 3) {
+    case 1:
+      year = "A";
+      break;
+    case 2:
+      year = "B";
+      break;
+    default:
+      year = "C";
+  }
+  return year;
+}
 export function addTextToBody(text) {
   const div = document.createElement('div');
-  div.textContent = text;
+  div.textContent = tinhNamABC(2025);
   document.body.appendChild(div);
 }

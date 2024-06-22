be9cadd15a550b96c6866e39e2a9c9ba2eb66161
Duy Tran <duytran31187@gmail.com>
2024-06-22 18:44:45 +0700

    aaaaaaa

--- assets/js/main-mobile.js
+++ assets/js/main-mobile.js
@@ -2,7 +2,6 @@ import { getTinhNamPhungVuInstant } from "./dist-lichphungvu/index.js";
 
 var namPhungvu = getTinhNamPhungVuInstant(2024);
 const fullYear = namPhungvu.getFullLichPhungVuTheoNam();
-const year2024 = new namPhungvu(2024);
 
 let count = 0;
 document.getElementById('holder3').innerHTML = JSON.stringify(fullYear);

ec6403a91703721c44896037284c9dd14fbe7808
Duy Tran <duytran31187@gmail.com>
2024-06-22 18:02:47 +0700

    refer

--- assets/js/main-mobile.js
+++ assets/js/main-mobile.js
@@ -1,4 +1,8 @@
+import { getTinhNamPhungVuInstant } from "./dist-lichphungvu/index.d.mts";
+
 var namPhungvu = getTinhNamPhungVuInstant(2024);
 const fullYear = namPhungvu.getFullLichPhungVuTheoNam();
+const year2024 = new namPhungvu(2024);
+
 let count = 0;
 document.getElementById('holder3').innerHTML = JSON.stringify(fullYear);
